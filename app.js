fc88d0c1b9b390c708e21bc02f5c9315833cc2f8
tippenein <tippenein@gmail.com>
2014-02-01 12:13:25 -0600

    oauth instead of passport

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // app.js
 
 var express   = require('express'),
-    passport  = require('passport'),
     request   = require('request'),
-//    OAuth     = require('oauth').OAuth
+    OAuth     = require('oauth').OAuth,
     config    = require('./config'),
-    TwitterStrategy = require('passport-twitter').Strategy;
+    _         = require('underscore');
+    // passport hides some things that I needed.. So I ditched that
 
 var app = express();
 
@@ -16,78 +16,96 @@ app.configure(function() {
   app.use(express.cookieParser() );
   app.use(express.bodyParser() );
   app.use(express.methodOverride() );
-  app.use(express.session({ secret: 'keyboard sloth' }));
-  app.use(passport.initialize());
-  app.use(passport.session());
+  app.use(express.session({secret: "keyboard sloth"}));
   app.use(app.router);
 });
 
-var callbackURL = "http://127.0.0.1:3000/auth/twitter/callback";
-// var oauthT = new OAuth(
-//     'https://api.twitter.com/oauth/request_token',
-//     'https://api.twitter.com/oauth/access_token',
-//     config.consumer_key,
-//     config.consumer_secret,
-//     '1.0',
-//     callbackURL,
-//     'HMAC-SHA1'
-//     );
+var callbackURL = config.site + "/auth/twitter/callback";
+console.log("callbackURL: " + callbackURL)
+var oauth = new OAuth(
+    'https://api.twitter.com/oauth/request_token',
+    'https://api.twitter.com/oauth/access_token',
+    config.consumer_key,
+    config.consumer_secret,
+    '1.0',
+    callbackURL,
+    'HMAC-SHA1'
+    );
 
-//var callback = config.site + ":" + config.port + "/callback";
-
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-passport.use(new TwitterStrategy({
-    consumerKey: config.consumer_key,
-    consumerSecret: config.consumer_secret,
-    callbackURL: callbackURL
-  },
-  function(token, tokenSecret, profile, done) {
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
-      return done(null, profile);
-    });
-  }
-));
-
+// -- index --
 app.get('/', function(req, res) {
-  res.render('index', {
-    title: config.title,
-    user: req.user });
+  if (!req.session.oauth) {
+    res.render('login', {title: config.title + " : Login"});
+  } else {
+    res.redirect('/search');
+  }
 });
 
-app.get('/search', function(req, res) {
-  var urlCall = config.searchResource + "?q=" + req.query.q;
-  console.log(passport)
-  var oauth = {
-      consumer_key: config.consumer_key,
-      consumer_secret: config.consumer_secret,
-      token: passport.session.oauth.access_token,
-      token_secret: passport.session.oauth.access_token_secret
-  };
-  request.get({
-      url: urlCall,
-      oauth: oauth,
-      json: true
-  }, function(err, response, body) {
-    console.log(body)
-    if (err) {console.log("meh:" + err); }
-    else {
-      res.render('index', { title:'Searched \"' + req.query.q,
-                            tweets: body.statuses });
+// -- login --
+app.get('/login', function(req, res) {
+  oauth.getOAuthRequestToken(function(err, tok, secret, data) {
+    if (err) {
+      console.log("err: " + err);
+    } else {
+      req.session.oauth = {};
+      req.session.oauth.token = tok;
+      req.session.oauth.token_secret = secret;
+      res.redirect(config.tokenResource + tok);
     }
   });
 });
 
-app.get('/login', function(req, res) {
-  res.render('login', { title: "login to Tweetmap-funtime"
-                      , user: req.user });
+// -- callback --
+app.get('/callback', function(req, res) {
+  if (!_.isUndefined(req.session.oauth)) {
+    req.session.oauth.verifier = req.query.oauth_verifier;
+    var oa = req.session.oauth;
+    oauth.getOAuthAccessToken(oa.token, oa.token_secret, oa.verifier, function(err, tok, secret, data) {
+      if (err) {
+        console.log("err: " + err);
+      } else {
+        req.session.oauth.screen_name = data.screen_name;
+        req.session.oauth.access_token = tok;
+        req.session.oauth.access_token_secret = secret;
+        res.redirect('/');
+      }
+    });
+  } else {
+      res.redirect('/');
+  }
+})
+
+// -- search --
+app.get('/search', function(req, res) {
+  if (!_.isUndefined(req.session.oauth)) {
+    var oauth = {
+        consumer_key: config.consumer_key,
+        consumer_secret: config.consumer_secret,
+        token: config.access_token,
+        token_secret: config.token_secret
+    };
+    var q = req.query.q;
+    if (q) {
+      var urlCall = config.searchResource + "?q=" + q;
+      request.get({
+          url: urlCall,
+          oauth: oauth,
+          json: true
+      }, function(err, response, body) {
+        console.log(body)
+        if (err) {console.log("meh:" + err); }
+        else {
+          res.render('index', { title:'Searched \"' + q,
+                                tweets: body.statuses });
+        }
+      });
+    }
+  } else {
+    // not authenticated
+    res.redirect('/');
+  }
 });
+
 
 app.get('/logout', function(req, res) {
   req.logout();
@@ -99,10 +117,10 @@ app.get('/tweetmap/:user', function(req,res) {
 })
 
 //passport specific routes
-app.get('/auth/twitter', passport.authenticate('twitter'));
-app.get('/auth/twitter/callback',
-  passport.authenticate('twitter', { successRedirect: '/',
-                                     failureRedirect: '/auth/twitter' }));
+//app.get('/auth/twitter', passport.authenticate('twitter'));
+//app.get('/auth/twitter/callback',
+//   passport.authenticate('twitter', { successRedirect: '/',
+//                                      failureRedirect: '/auth/twitter' }));
 
 app.listen(process.env.PORT || 3000);
 
--- config.js
+++ config.js
@@ -1,17 +1,19 @@
 switch(process.env.NODE_ENV) {
   case 'dev':
-    exports.site = 'localhost';
-    exports.port = 8000;
+    exports.site = 'http://127.0.0.1:3000';
     break;
   default:
     exports.site = "http://fierce-everglades-3598.herokuapp.com";
     break;
 }
-exports.title = "Tweet App Thingy!"
+exports.title = "Tweet App Thingy!";
 exports.consumer_key = 'cKL8vtoAWxFKxEi5gaAN2A';
 exports.consumer_secret = 'A5WN7bzAexqCsJeUcRKOW598nocy30fyvYqpe8yuU';
+exports.access_token = '16987909-KvjBMG8qtH4joJ5WBHOcbdUS26xgjFLHUqaoIisqg';
+exports.token_secret = 'jE5u2BdG6tHzPnSAEePiB2XZ0ndIkjkeMkgutRjcxw85C';
+
 // https://dev.twitter.com/docs/api/1.1/get/search/tweets
 exports.searchResource = 'https://api.twitter.com/1.1/search/tweets.json';
 // https://dev.twitter.com/docs/api/1.1/get/statuses/user_timeline
 exports.userTimelineResource = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
-
+exports.tokenResource = 'https://twitter.com/oauth/authenticate?oauth_token=';
